abddae53a547502b2918c179983784d71fb4f409
Maximilian Behrendt <maximilian.behrendt@envite.de>
2025-05-28 16:38:22 +0200

    initial commit

---
+++ src/main/java/info/maxbehr/bpm/camundatesting/CamundaTestingApplication.java
@@ -0,0 +1,13 @@
+package info.maxbehr.bpm.camundatesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/maxbehr/bpm/camundatesting/CamundaTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package info.maxbehr.bpm.camundatesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
